9b1e6ad2fceb60f5effe27d1d9833a187b1a0b49
KennyT <65835355+tietkenneth@users.noreply.github.com>
2024-07-18 16:24:38 -0700

    Connecting backend with Choreo

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import api from "../api";
 import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
-import "../styles/Form.css"
+import "../styles/Form.css";
+import LoadingIndicator from "./LoadingIndicator";
 
 function Form({ route, method }) {
   const [username, setUsername] = useState("");
@@ -50,6 +51,7 @@ function Form({ route, method }) {
         onChange={(e) => setPassword(e.target.value)}
         placeholder="Password"
       />
+      { loading && <LoadingIndicator/> }
       <button className="form-button" type="submit">
         {name}
       </button>
